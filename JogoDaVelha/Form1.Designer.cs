17f3c3b2e65d319f8ce4d08bfa31d3336f55de51
Matheus Bruno <matheusferreirabruno@hotmail.com>
2022-04-11 09:16:55 -0300

    Desabilitando maximizar e redimensionar form

--- JogoDaVelha/Form1.Designer.cs
+++ JogoDaVelha/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -56,81 +57,90 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(0, 26);
+            this.button1.Location = new System.Drawing.Point(0, 20);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(124, 110);
+            this.button1.Size = new System.Drawing.Size(108, 82);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(130, 26);
+            this.button2.Location = new System.Drawing.Point(114, 20);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(124, 110);
+            this.button2.Size = new System.Drawing.Size(108, 82);
             this.button2.TabIndex = 1;
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(260, 26);
+            this.button3.Location = new System.Drawing.Point(228, 20);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(124, 110);
+            this.button3.Size = new System.Drawing.Size(108, 82);
             this.button3.TabIndex = 2;
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(0, 139);
+            this.button4.Location = new System.Drawing.Point(0, 104);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(124, 110);
+            this.button4.Size = new System.Drawing.Size(108, 82);
             this.button4.TabIndex = 3;
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(130, 139);
+            this.button5.Location = new System.Drawing.Point(114, 104);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(124, 110);
+            this.button5.Size = new System.Drawing.Size(108, 82);
             this.button5.TabIndex = 4;
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(260, 139);
+            this.button6.Location = new System.Drawing.Point(228, 104);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(124, 110);
+            this.button6.Size = new System.Drawing.Size(108, 82);
             this.button6.TabIndex = 5;
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(0, 255);
+            this.button7.Location = new System.Drawing.Point(0, 191);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(124, 110);
+            this.button7.Size = new System.Drawing.Size(108, 82);
             this.button7.TabIndex = 6;
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(130, 256);
+            this.button8.Location = new System.Drawing.Point(114, 192);
+            this.button8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(124, 110);
+            this.button8.Size = new System.Drawing.Size(108, 82);
             this.button8.TabIndex = 7;
             this.button8.UseVisualStyleBackColor = true;
             this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(260, 255);
+            this.button9.Location = new System.Drawing.Point(228, 191);
+            this.button9.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(124, 110);
+            this.button9.Size = new System.Drawing.Size(108, 82);
             this.button9.TabIndex = 8;
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button9_Click);
@@ -139,9 +149,11 @@
             // 
             this.jogadorOne.Controls.Add(this.nomeJogador2);
             this.jogadorOne.Controls.Add(this.LbPontuacaoOne);
-            this.jogadorOne.Location = new System.Drawing.Point(9, 400);
+            this.jogadorOne.Location = new System.Drawing.Point(8, 300);
+            this.jogadorOne.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.jogadorOne.Name = "jogadorOne";
-            this.jogadorOne.Size = new System.Drawing.Size(124, 49);
+            this.jogadorOne.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.jogadorOne.Size = new System.Drawing.Size(108, 37);
             this.jogadorOne.TabIndex = 9;
             this.jogadorOne.TabStop = false;
             this.jogadorOne.Text = "jogador Um";
@@ -149,18 +161,18 @@
             // nomeJogador2
             // 
             this.nomeJogador2.AutoSize = true;
-            this.nomeJogador2.Location = new System.Drawing.Point(24, 229);
+            this.nomeJogador2.Location = new System.Drawing.Point(21, 172);
             this.nomeJogador2.Name = "nomeJogador2";
-            this.nomeJogador2.Size = new System.Drawing.Size(109, 20);
+            this.nomeJogador2.Size = new System.Drawing.Size(86, 15);
             this.nomeJogador2.TabIndex = 1;
             this.nomeJogador2.Text = "nomeJogador2";
             // 
             // LbPontuacaoOne
             // 
             this.LbPontuacaoOne.AutoSize = true;
-            this.LbPontuacaoOne.Location = new System.Drawing.Point(6, 23);
+            this.LbPontuacaoOne.Location = new System.Drawing.Point(5, 17);
             this.LbPontuacaoOne.Name = "LbPontuacaoOne";
-            this.LbPontuacaoOne.Size = new System.Drawing.Size(17, 20);
+            this.LbPontuacaoOne.Size = new System.Drawing.Size(13, 15);
             this.LbPontuacaoOne.TabIndex = 0;
             this.LbPontuacaoOne.Text = "0";
             // 
@@ -168,9 +180,11 @@
             // 
             this.jogadorTwo.Controls.Add(this.label1);
             this.jogadorTwo.Controls.Add(this.LbPontuacaoTwo);
-            this.jogadorTwo.Location = new System.Drawing.Point(139, 400);
+            this.jogadorTwo.Location = new System.Drawing.Point(122, 300);
+            this.jogadorTwo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.jogadorTwo.Name = "jogadorTwo";
-            this.jogadorTwo.Size = new System.Drawing.Size(124, 49);
+            this.jogadorTwo.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.jogadorTwo.Size = new System.Drawing.Size(108, 37);
             this.jogadorTwo.TabIndex = 10;
             this.jogadorTwo.TabStop = false;
             this.jogadorTwo.Text = "jogador Dois";
@@ -178,18 +192,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 229);
+            this.label1.Location = new System.Drawing.Point(21, 172);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.Size = new System.Drawing.Size(38, 15);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
             // LbPontuacaoTwo
             // 
             this.LbPontuacaoTwo.AutoSize = true;
-            this.LbPontuacaoTwo.Location = new System.Drawing.Point(6, 23);
+            this.LbPontuacaoTwo.Location = new System.Drawing.Point(5, 17);
             this.LbPontuacaoTwo.Name = "LbPontuacaoTwo";
-            this.LbPontuacaoTwo.Size = new System.Drawing.Size(17, 20);
+            this.LbPontuacaoTwo.Size = new System.Drawing.Size(13, 15);
             this.LbPontuacaoTwo.TabIndex = 0;
             this.LbPontuacaoTwo.Text = "0";
             // 
@@ -197,9 +211,11 @@
             // 
             this.Empate.Controls.Add(this.label2);
             this.Empate.Controls.Add(this.LbEmpate);
-            this.Empate.Location = new System.Drawing.Point(269, 400);
+            this.Empate.Location = new System.Drawing.Point(235, 300);
+            this.Empate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Empate.Name = "Empate";
-            this.Empate.Size = new System.Drawing.Size(124, 49);
+            this.Empate.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Empate.Size = new System.Drawing.Size(108, 37);
             this.Empate.TabIndex = 11;
             this.Empate.TabStop = false;
             this.Empate.Text = "Empate";
@@ -207,18 +223,18 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 229);
+            this.label2.Location = new System.Drawing.Point(21, 172);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.Size = new System.Drawing.Size(38, 15);
             this.label2.TabIndex = 1;
             this.label2.Text = "label2";
             // 
             // LbEmpate
             // 
             this.LbEmpate.AutoSize = true;
-            this.LbEmpate.Location = new System.Drawing.Point(6, 23);
+            this.LbEmpate.Location = new System.Drawing.Point(5, 17);
             this.LbEmpate.Name = "LbEmpate";
-            this.LbEmpate.Size = new System.Drawing.Size(17, 20);
+            this.LbEmpate.Size = new System.Drawing.Size(13, 15);
             this.LbEmpate.TabIndex = 0;
             this.LbEmpate.Text = "0";
             // 
@@ -233,17 +249,20 @@
             this.groupBox1.Controls.Add(this.button8);
             this.groupBox1.Controls.Add(this.button6);
             this.groupBox1.Controls.Add(this.button7);
-            this.groupBox1.Location = new System.Drawing.Point(9, 12);
+            this.groupBox1.Location = new System.Drawing.Point(8, 9);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(384, 380);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(336, 285);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(139, 455);
+            this.btnReset.Location = new System.Drawing.Point(122, 341);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(124, 40);
+            this.btnReset.Size = new System.Drawing.Size(108, 30);
             this.btnReset.TabIndex = 12;
             this.btnReset.TabStop = false;
             this.btnReset.Text = "Resetar";
@@ -253,14 +272,18 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(405, 501);
+            this.ClientSize = new System.Drawing.Size(354, 376);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.Empate);
             this.Controls.Add(this.jogadorTwo);
             this.Controls.Add(this.jogadorOne);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Jogo da Velha";
             this.jogadorOne.ResumeLayout(false);
